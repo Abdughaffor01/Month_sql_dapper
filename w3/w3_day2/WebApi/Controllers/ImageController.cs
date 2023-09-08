c473dd77c5ce50bd8097fa316a0ee426d6a10e37
Abdughaffor01 <ghaffor987849660@gmail.com>
2023-09-08 15:07:21 +0500

    dz

---
+++ w3/w3_day2/WebApi/Controllers/ImageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ImageController : ControllerBase
+{
+    private readonly IWebHostEnvironment _environment;
+    public ImageController(IWebHostEnvironment environment)=>_environment=environment;
+
+    [HttpPost("Upload")]
+    public string UPloadImage(IFormFile file) {
+        string fullpath = Path.Combine(_environment.WebRootPath, "images",file.FileName);
+        using var stream=System.IO.File.Create(fullpath);
+        file.CopyTo(stream);
+        return fullpath;
+    }
+}
+
+    
